c1fb3d88dee09db62e5f57229acd9fcde53439f4
JonaDurante <jonathanduranteg@gmail.com>
2023-12-01 18:37:59 -0300

    Estructuración

---
+++ TostTest/TostTest/Controllers/Auth.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TostTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+        // GET: api/<Auth>
+        [HttpGet]
+        public IActionResult Authorize()
+        {
+            return Ok(new string[] { "Conectar" });
+        }
+
+
+    }
+}
---
+++ TostTest/TostTestData/Contexts/TostTestContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TostTest.App.Contexts
+{
+    public class TostTestContext : DbContext
+    {
+        public TostTestContext(DbContextOptions<TostTestContext> options) : base(options)
+        {
+                
+        }
+    }
+}
